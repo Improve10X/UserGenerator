1f3d95981f0477e591f0741e862d6d4cbba447ab
LENOVO <lakshmi.sannithi@improve10x.com>
2023-06-12 12:31:39 +0530

    I added Some Items in Person class

--- app/src/main/java/com/improve10x/usergenerator/model/Person.java
+++ app/src/main/java/com/improve10x/usergenerator/model/Person.java
@@ -1,4 +1,15 @@
 package com.improve10x.usergenerator.model;
 
 public class Person {
+
+    private String name;
+    private Integer age;
+    private String job;
+    private Integer incomeUSD;
+    private Integer creditScore;
+    private String ccNumber;
+    private Boolean married;
+    private Boolean hasChildren;
+    private Integer height;
+    private Float weight;
 }
