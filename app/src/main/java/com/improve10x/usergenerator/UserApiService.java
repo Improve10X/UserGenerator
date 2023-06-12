7604307dc320992aeab9a73b631ba9eb77eab45d
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-06-12 12:56:56 +0530

    Created Interface class

---
+++ app/src/main/java/com/improve10x/usergenerator/UserApiService.java
@@ -0,0 +1,14 @@
+package com.improve10x.usergenerator;
+
+import com.improve10x.usergenerator.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UserApiService {
+
+    @GET("person/10")
+    Call<List<User>> fetchUsers();
+}
