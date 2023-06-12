08e578def11e30f728670e3039042dd83f2a2fdf
LENOVO <lakshmi.sannithi@improve10x.com>
2023-06-12 12:53:42 +0530

    I completed model class

---
+++ app/src/main/java/com/improve10x/usergenerator/model/Address.java
@@ -0,0 +1,86 @@
+package com.improve10x.usergenerator.model;
+
+public class Address {
+
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String country;
+    private String zip;
+    private Integer geonameId;
+    private String phoneNumber;
+    private String ipAddress;
+    private String countryCode;
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Integer getGeonameId() {
+        return geonameId;
+    }
+
+    public void setGeonameId(Integer geonameId) {
+        this.geonameId = geonameId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}
--- app/src/main/java/com/improve10x/usergenerator/model/Person.java
+++ app/src/main/java/com/improve10x/usergenerator/model/Person.java
@@ -12,4 +12,183 @@ public class Person {
     private Boolean hasChildren;
     private Integer height;
     private Float weight;
+    private String eyeColor;
+    private String email;
+    private String gender;
+    private Boolean hasDegree;
+    private String bloodType;
+    private String userName;
+    private Float politicalLeaning;
+    private String religion;
+    private Address address;
+    private String doB;
+    private Float gpa;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getIncomeUSD() {
+        return incomeUSD;
+    }
+
+    public void setIncomeUSD(Integer incomeUSD) {
+        this.incomeUSD = incomeUSD;
+    }
+
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public Boolean getHasChildren() {
+        return hasChildren;
+    }
+
+    public void setHasChildren(Boolean hasChildren) {
+        this.hasChildren = hasChildren;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getHasDegree() {
+        return hasDegree;
+    }
+
+    public void setHasDegree(Boolean hasDegree) {
+        this.hasDegree = hasDegree;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Float getPoliticalLeaning() {
+        return politicalLeaning;
+    }
+
+    public void setPoliticalLeaning(Float politicalLeaning) {
+        this.politicalLeaning = politicalLeaning;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getDoB() {
+        return doB;
+    }
+
+    public void setDoB(String doB) {
+        this.doB = doB;
+    }
+
+    public Float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(Float gpa) {
+        this.gpa = gpa;
+    }
 }
