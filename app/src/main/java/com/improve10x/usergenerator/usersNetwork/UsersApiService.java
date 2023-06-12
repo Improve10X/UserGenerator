283b39d00c5a1edf7fab97d37cb1e800bf267392
Sumanth krishna M <sumanth.krishna@improve10x.com>
2023-06-12 16:12:02 +0530

    I implemented POST, DELETE method Interface

--- app/src/main/java/com/improve10x/usergenerator/usersNetwork/UsersApiService.java
+++ app/src/main/java/com/improve10x/usergenerator/usersNetwork/UsersApiService.java
@@ -5,10 +5,20 @@ import com.improve10x.usergenerator.model.Person;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface UsersApiService {
 
     @GET("venusTemplates")
     Call<List<Person>> getUsersDetails();
+
+    @POST("venusTemplates")
+    Call<Person> createPersonDetails(@Body Person person);
+
+    @DELETE("venusTemplates /{id}")
+    Call<Void> deletePersonDetails(@Path("id") String id);
 }
