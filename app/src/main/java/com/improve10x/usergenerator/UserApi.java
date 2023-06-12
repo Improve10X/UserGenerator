fbadc3004d861530b78776fe02f4626a07e10319
SivaKumar <sivakumar.g@improve10x.com>
2023-06-12 15:11:13 +0530

    i wrote the test case.and passed testcase.

--- app/src/main/java/com/improve10x/usergenerator/UserApi.java
+++ app/src/main/java/com/improve10x/usergenerator/UserApi.java
@@ -1,17 +1,25 @@
 package com.improve10x.usergenerator;
 
+import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserApi {
+    public UserApiService createUserApiService() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-    public UserApiService createUserApiService(){
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://peoplegeneratorapi.live/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         UserApiService userApiService = retrofit.create(UserApiService.class);
-        return userApiService ;
+        return userApiService;
     }
 }
--- app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
+++ app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.google.gson.Gson;
+import com.improve10x.usergenerator.model.Person;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +21,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getUserDetails() throws IOException {
+        UserApiService userApiService = new UserApi().createUserApiService();
+        Call<List<Person>> call = userApiService.getUsersDetails();
+        List<Person> person = call.execute().body();
+        assertNotNull(person);
+        assertFalse(person.isEmpty());
+        System.out.println(new Gson().toJson(person));
     }
 }
