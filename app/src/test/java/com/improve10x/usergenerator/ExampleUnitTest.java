ff8f46a6b18307078ea525648ecb350ac7288272
Omprakash <omprakash.mopuri@improve10x.com>
2023-06-12 14:46:06 +0530

    Wrote the test case for peoplegenarateapi in ExampleUnitTest.java

--- app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
+++ app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
@@ -4,6 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+import com.google.gson.Gson;
+import com.improve10x.usergenerator.model.User;
+import com.improve10x.usergenerator.network.UserApi;
+import com.improve10x.usergenerator.network.UserApiService;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +24,16 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getUsers() throws IOException {
+        UserApi userApi = new UserApi();
+        UserApiService userApiService = userApi.createUserApiService();
+        Call<List<User>> call = userApiService.fetchUsers();
+        List<User> users = call.execute().body();
+        assertNotNull(users);
+        assertFalse(users.isEmpty());
+        System.out.println(new Gson().toJson(users));
     }
 }
