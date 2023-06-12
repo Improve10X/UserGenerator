353d66630bf304acf3c3ecf1fd5166589d3f26fc
roopa <roopa.peddinti@improve10x.com>
2023-06-12 13:02:17 +0530

    Created Model Classes

---
+++ app/src/main/java/com/improve10x/usergenerator/model/Address.java
@@ -0,0 +1,13 @@
+package com.improve10x.usergenerator.model;
+
+public class Address {
+    private String streetAddress;
+    private String city;
+    private String  state;
+    private String country;
+    private String zip;
+    private Integer geoNameId;
+    private String phoneNumber;
+    private String ipAddress;
+    private String countyCode;
+}
---
+++ app/src/main/java/com/improve10x/usergenerator/model/User.java
@@ -0,0 +1,27 @@
+package com.improve10x.usergenerator.model;
+
+import android.location.Address;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String job;
+    private Integer incomeUSD;
+    private Integer creditScore;
+    private String ccNumber;
+    private Boolean married;
+    private Boolean hasChildren;
+    private Integer height;
+    private Float weight;
+    private String eyeColor;
+    private String email;
+    private String gender;
+    private Boolean hasDegree;
+    private String bloodType;
+    private String userName;
+    private String politicalLeaning;
+    private String religion;
+    private Address address;
+    private String doB;
+    private Float gpa;
+}
