bbf5913375988c4d5037208728040a3e75ba6cd5
sunitha <sunithapraveenkumar@gmail.com>
2023-06-12 16:11:14 +0530

    TestCase Completed for peopleData

--- app/src/main/java/com/improve10x/usergenerator/model/Address.java
+++ app/src/main/java/com/improve10x/usergenerator/model/Address.java
@@ -1,13 +1,25 @@
 package com.improve10x.usergenerator.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Address {
     private String streetAddress;
     private String city;
     private String  state;
     private String country;
     private String zip;
-    private Integer geoNameId;
+    private Integer geonameId;
     private String phoneNumber;
     private String ipAddress;
-    private String countyCode;
+    private String countryCode;
 }
+
+
+//"streetAddress": "142 Ted Greens",
+//      "city": "Beirut",
+//      "state": "Nabatîyé",
+//      "country": "Lebanon",
+//      "zip": "1161",
+//      "geonameId": 278832,
+//      "phoneNumber": "+961 83182898",
+//      "ipAddress": "102.195.45.179"
--- app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
+++ app/src/test/java/com/improve10x/usergenerator/ExampleUnitTest.java
@@ -4,6 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.google.gson.Gson;
+import com.improve10x.usergenerator.model.User;
+import com.improve10x.usergenerator.network.PeopleApi;
+import com.improve10x.usergenerator.network.PeopleService;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +23,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getPeopleData() throws IOException {
+        PeopleService peopleService = new PeopleApi().createPeopleService();
+        Call<List<User>> call = peopleService.fetchPeopleGenerator();
+        List<User> users = call.execute().body();
+        assertNotNull(users);
+        assertFalse(users.isEmpty());
+        System.out.println(new Gson().toJson(users));
     }
 }
