fd7424b9aab29eb6abbed9d995cfaa5b5de4d473
Renuka Challa <challarenuka20@gmail.com>
2023-06-12 11:18:20 +0530

    Created SplashActivity and navigated to dummy.

---
+++ app/src/main/java/com/improve10x/usergenerator/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.improve10x.usergenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+                    finish();
+                    Intent mainIntent = new Intent(this, MainActivity.class);
+                    startActivity(mainIntent);
+                }, 3000
+        );
+    }
+}
