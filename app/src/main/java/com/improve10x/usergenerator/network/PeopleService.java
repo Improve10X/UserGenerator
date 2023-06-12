3bf9b6ae559ead38500d4e00102ceae5739c8e73
Manohar <manohar.mangali@improve10x.com>
2023-06-12 14:53:47 +0530

    I created interface class

---
+++ app/src/main/java/com/improve10x/usergenerator/network/PeopleService.java
@@ -0,0 +1,14 @@
+package com.improve10x.usergenerator.network;
+
+import com.improve10x.usergenerator.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface PeopleService {
+    @GET("person/10")
+    Call<List<User>> fetchPeopleGenerator();
+}
