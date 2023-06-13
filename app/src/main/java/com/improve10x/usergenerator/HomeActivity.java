2e9c6ee69dbae9b39d0f31b8fdbfa1edfe90f227
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-13 14:52:15 +0530

    created RandomUserGenerateActivity and navigated homeScreen to RandomUserGenerateActivity

--- app/src/main/java/com/improve10x/usergenerator/HomeActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/HomeActivity.java
@@ -2,13 +2,28 @@ package com.improve10x.usergenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.improve10x.usergenerator.databinding.ActivityHomeBinding;
+import com.improve10x.usergenerator.randomusergenerater.RandomUserGenerateActivity;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleRandomUsersBtn();
+    }
+
+    private void handleRandomUsersBtn() {
+        binding.gruBtn.setOnClickListener(v -> {
+            Intent randomUsersIntent = new Intent(this, RandomUserGenerateActivity.class);
+            startActivity(randomUsersIntent);
+        });
     }
 }
---
+++ app/src/main/java/com/improve10x/usergenerator/randomusergenerater/RandomUserGenerateActivity.java
@@ -0,0 +1,16 @@
+package com.improve10x.usergenerator.randomusergenerater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.improve10x.usergenerator.R;
+
+public class RandomUserGenerateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_user_generate);
+    }
+}
