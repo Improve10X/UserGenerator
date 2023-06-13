0f111c6c1a482d83a321b071f4134764fa33faec
LENOVO <lakshmi.sannithi@improve10x.com>
2023-06-13 12:05:59 +0530

    I added recycler view and build features

--- app/src/main/java/com/improve10x/usergenerator/RandomUsersActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/RandomUsersActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.improve10x.usergenerator.databinding.ActivityRandomUsersBinding;
+import com.improve10x.usergenerator.model.User;
+
+import java.util.List;
+
 public class RandomUsersActivity extends AppCompatActivity {
+
+    ActivityRandomUsersBinding binding;
+
+    private List<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random_users);
+        binding = ActivityRandomUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Random User");
     }
 }
