a9dc09c3e0ada9670d09f24a3d3606407faec2a9
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-13 16:59:12 +0530

    Implemented setupAdapter() and setupRandomUserRv() in RandomUserGenerateActivity

--- app/src/main/java/com/improve10x/usergenerator/randomusergenerater/RandomUserGenerateActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/randomusergenerater/RandomUserGenerateActivity.java
@@ -1,16 +1,39 @@
 package com.improve10x.usergenerator.randomusergenerater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.improve10x.usergenerator.R;
+import com.improve10x.usergenerator.databinding.ActivityRandomUserGenerateBinding;
+import com.improve10x.usergenerator.modelclass.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RandomUserGenerateActivity extends AppCompatActivity {
+
+    private ActivityRandomUserGenerateBinding binding;
+    private RandomUserGenerateAdapter randomUserGenerateAdapter;
+    private List<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random_user_generate);
+        binding = ActivityRandomUserGenerateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Random Users");
+        setupAdapter();
+        setupRandomUsersRv();
+    }
+
+    private void setupAdapter() {
+        randomUserGenerateAdapter = new RandomUserGenerateAdapter(users);
+    }
+
+    private void setupRandomUsersRv() {
+        binding.randomUserGenerateRv.setLayoutManager(new LinearLayoutManager(this));
+        binding.randomUserGenerateRv.setAdapter(randomUserGenerateAdapter);
     }
 }
