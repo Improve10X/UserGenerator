f657bcb7cf9c1931af61239059b768aa8a486cdb
sunitha <sunithapraveenkumar@gmail.com>
2023-06-13 16:48:48 +0530

    I Implemented onCreateViewHolder In RandomUsersAdapter Class.

--- app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
+++ app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
@@ -1,16 +1,24 @@
 package com.improve10x.usergenerator.randomuser;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.improve10x.usergenerator.databinding.RandomUserItemBinding;
+
+import retrofit2.Call;
+
 public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolder> {
+
 
     @NonNull
     @Override
     public RandomUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        RandomUserItemBinding randomUserItemBinding = RandomUserItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent,false);
+        RandomUserViewHolder randomUserViewHolder = new RandomUserViewHolder(randomUserItemBinding);
+        return randomUserViewHolder;
     }
 
     @Override
