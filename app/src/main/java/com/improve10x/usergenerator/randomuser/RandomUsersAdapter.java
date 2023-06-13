c4f48314c93dd6d73d33f3c8681f786c4d0d5d42
roopa <roopa.peddinti@improve10x.com>
2023-06-13 16:22:58 +0530

    Created RandomUsersAdapter class in randomuser package.

---
+++ app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
@@ -0,0 +1,25 @@
+package com.improve10x.usergenerator.randomuser;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolder> {
+
+    @NonNull
+    @Override
+    public RandomUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomUserViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
