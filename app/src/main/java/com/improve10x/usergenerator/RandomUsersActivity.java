a530b3f42a38b2de86e19857f02acf4953008083
Manohar <manohar.mangali@improve10x.com>
2023-06-13 12:21:45 +0530

    I created new branch and RandomUsersActivity created

---
+++ app/src/main/java/com/improve10x/usergenerator/RandomUsersActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.usergenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_users);
+    }
+}
