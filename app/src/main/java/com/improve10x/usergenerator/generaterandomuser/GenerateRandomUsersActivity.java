1bbaeb97179a7580001502fec69de5e761e86150
Vinodkumar M <vinod.kumar@improve10x.com>
2023-06-13 13:09:47 +0530

    Implemented Recyclerview and Adapter in GenerateRandomUsersActivity.

--- app/src/main/java/com/improve10x/usergenerator/generaterandomuser/GenerateRandomUsersActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/generaterandomuser/GenerateRandomUsersActivity.java
@@ -1,12 +1,21 @@
 package com.improve10x.usergenerator.generaterandomuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.improve10x.usergenerator.databinding.ActivityGenerateRandomUsersBinding;
+import com.improve10x.usergenerator.model.User;
+
+import java.util.ArrayList;
 
 public class GenerateRandomUsersActivity extends AppCompatActivity {
+
+    private ArrayList<User> generateRandomUsers = new ArrayList<>();
+
+    private GenerateRandomUsersAdapter generateRandomUsersAdapter;
 
     private ActivityGenerateRandomUsersBinding binding;
 
@@ -16,5 +25,18 @@ public class GenerateRandomUsersActivity extends AppCompatActivity {
         binding = ActivityGenerateRandomUsersBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Random Users");
+        setupGenerateRandomUsersRv();
+        setupGenerateAdapter();
+    }
+
+    private void setupGenerateAdapter() {
+        generateRandomUsersAdapter = new GenerateRandomUsersAdapter();
+        generateRandomUsersAdapter.setGenerateRandomUsers(generateRandomUsers);
+    }
+
+    private void setupGenerateRandomUsersRv() {
+        binding.generateRandomUsersRv.setLayoutManager(new LinearLayoutManager(this));
+        binding.generateRandomUsersRv.setAdapter(generateRandomUsersAdapter);
+
     }
 }
