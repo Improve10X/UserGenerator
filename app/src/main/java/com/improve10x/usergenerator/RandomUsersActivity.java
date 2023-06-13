bbe2497451071e3a5a1dac4ad95b0b1a78034633
Sumanth krishna M <sumanth.krishna@improve10x.com>
2023-06-13 11:52:12 +0530

    I created a random_users_item.xml

--- app/src/main/java/com/improve10x/usergenerator/RandomUsersActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/RandomUsersActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RandomUserActivity extends AppCompatActivity {
+public class RandomUsersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random_user);
+        setContentView(R.layout.activity_random_users);
+        getSupportActionBar().setTitle("Random User");
     }
 }
