c62dfda0a42b6d3189d2dd1d2f68f6734169ec9f
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-06-13 11:46:44 +0530

    I created UserApi class

---
+++ app/src/main/java/com/improve10x/usergenerator/network/UserApi.java
@@ -0,0 +1,30 @@
+package com.improve10x.usergenerator.network;
+
+import com.improve10x.usergenerator.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class UserApi {
+
+    public UserService createUserService() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.USER_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        UserService userService = retrofit.create(UserService.class);
+        return userService;
+    }
+}
