3620737c4ef138e21bdd81df868e673e48558b9a
Harsha <shreeharsha.a@improve10x.com>
2023-06-13 13:56:29 +0530

    I implemented DataBinding and Created a adapter and ViewHolder class

--- app/src/main/java/com/improve10x/usergenerator/RandomUserAdapter.java
+++ app/src/main/java/com/improve10x/usergenerator/RandomUserAdapter.java
@@ -13,29 +13,19 @@ import java.util.ArrayList;
 
 public class RandomUserAdapter extends RecyclerView.Adapter<RandomViewHolder> {
 
-    private ArrayList<User> users;
-
-    public void setUsers(ArrayList<User> users) {
-        this.users = users;
-        notifyDataSetChanged();
-    }
 
     @NonNull
     @Override
     public RandomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        RandomUserItemBinding randomUserItemBinding = RandomUserItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        RandomViewHolder randomViewHolder = new RandomViewHolder(randomUserItemBinding);
-        return randomViewHolder;
+        return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RandomViewHolder holder, int position) {
-        User user = users.get(position);
-        holder.randomUserItemBinding.setUser(user);
     }
 
     @Override
     public int getItemCount() {
-        return users.size();
+        return 0;
     }
 }
