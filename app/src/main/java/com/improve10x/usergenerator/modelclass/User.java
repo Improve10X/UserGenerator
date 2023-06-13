5b2352a8d432db45a73b7546e3f551edce3a9d31
Anupama <anupama.lakshmi@Improve10x.com>
2023-06-13 16:50:23 +0530

    Created RandomUserGenerateAdapterClass and implemented it's functionality.

--- app/src/main/java/com/improve10x/usergenerator/modelclass/User.java
+++ app/src/main/java/com/improve10x/usergenerator/modelclass/User.java
@@ -58,5 +58,173 @@ public class User {
         this.dOB = dOB;
         this.gpa = gpa;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getIncomeUsd() {
+        return incomeUsd;
+    }
+
+    public void setIncomeUsd(Integer incomeUsd) {
+        this.incomeUsd = incomeUsd;
+    }
+
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public Boolean getHasChildren() {
+        return hasChildren;
+    }
+
+    public void setHasChildren(Boolean hasChildren) {
+        this.hasChildren = hasChildren;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getHasDegree() {
+        return hasDegree;
+    }
+
+    public void setHasDegree(Boolean hasDegree) {
+        this.hasDegree = hasDegree;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Float getPoliticalLeaning() {
+        return politicalLeaning;
+    }
+
+    public void setPoliticalLeaning(Float politicalLeaning) {
+        this.politicalLeaning = politicalLeaning;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(String dOB) {
+        this.dOB = dOB;
+    }
+
+    public Float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(Float gpa) {
+        this.gpa = gpa;
+    }
 }
 
---
+++ app/src/main/java/com/improve10x/usergenerator/randomusergenerater/RandomUserGenerateAdapter.java
@@ -0,0 +1,52 @@
+package com.improve10x.usergenerator.randomusergenerater;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.improve10x.usergenerator.databinding.RandomUsersItemBinding;
+import com.improve10x.usergenerator.modelclass.Address;
+import com.improve10x.usergenerator.modelclass.User;
+
+import java.util.List;
+
+public class RandomUserGenerateAdapter extends RecyclerView.Adapter<RandomUserGenerateViewHolder> {
+
+    private List<User> users;
+
+    public RandomUserGenerateAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    void setData(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RandomUserGenerateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RandomUsersItemBinding binding = RandomUsersItemBinding.inflate(LayoutInflater.from(parent.getContext()),parent, false);
+        RandomUserGenerateViewHolder randomUserGenerateViewHolder = new RandomUserGenerateViewHolder(binding);
+        return randomUserGenerateViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomUserGenerateViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.randomUsersItemBinding.nameTxt.setText(user.getName());
+        holder.randomUsersItemBinding.jobTxt.setText(user.getJob());
+        holder.randomUsersItemBinding.emailTxt.setText(user.getEmail());
+        holder.randomUsersItemBinding.incomeTxt.setText(String.valueOf(user.getIncomeUsd()));
+        holder.randomUsersItemBinding.creditScoreTxt.setText(String.valueOf(user.getCreditScore()));
+        holder.randomUsersItemBinding.addressTxt.setText(user.getAddress().getStreetAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+}
+
