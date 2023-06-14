8b444c57201a62da2b6e88179002178fca92c838
Manohar <manohar.mangali@improve10x.com>
2023-06-14 10:03:48 +0530

    I Implemented getter and setter methodsin model classes

--- app/src/main/java/com/improve10x/usergenerator/model/Address.java
+++ app/src/main/java/com/improve10x/usergenerator/model/Address.java
@@ -29,4 +29,76 @@ public class Address {
         this.ipAddress = ipAddress;
         this.countryCode = countryCode;
     }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Integer getGeoNameId() {
+        return geoNameId;
+    }
+
+    public void setGeoNameId(Integer geoNameId) {
+        this.geoNameId = geoNameId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
 }
--- app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
+++ app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
@@ -7,10 +7,15 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.improve10x.usergenerator.databinding.RandomUserItemBinding;
+import com.improve10x.usergenerator.model.User;
+
+import java.util.List;
 
 import retrofit2.Call;
 
 public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolder> {
+
+    List<User> users;
 
 
     @NonNull
@@ -23,6 +28,10 @@ public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolde
 
     @Override
     public void onBindViewHolder(@NonNull RandomUserViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.binding.nameTxt.setText(user.getName());
+        holder.binding.emailTxt.setText(user.getEmail());
+        holder.binding.incomeTxt.setText(user.getIncomeUsd());
 
     }
 
