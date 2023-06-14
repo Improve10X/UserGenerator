a709b81837bd04a74e9fa40c968a17df2773673d
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-14 12:20:34 +0530

    I wrote code for fetchRandomUsers()

--- app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersActivity.java
@@ -1,16 +1,62 @@
 package com.improve10x.usergenerator.randomuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.improve10x.usergenerator.R;
+import com.improve10x.usergenerator.databinding.ActivityRandomUsersBinding;
+import com.improve10x.usergenerator.model.User;
+import com.improve10x.usergenerator.network.randompeopleapi.PeopleApi;
+import com.improve10x.usergenerator.network.randompeopleapi.RandomPeopleService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RandomUsersActivity extends AppCompatActivity {
+
+    private ActivityRandomUsersBinding binding;
+    private List<User> users = new ArrayList<>();
+    private RandomUsersAdapter randomUsersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random_users);
+        binding = ActivityRandomUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupAdapter();
+        setupRandomUsersRv();
+        fetchRandomUsers();
+    }
+
+    private void setupAdapter () {
+        randomUsersAdapter = new RandomUsersAdapter(users);
+    }
+
+    private void setupRandomUsersRv() {
+        binding.randomUsersRv.setLayoutManager(new LinearLayoutManager(this));
+        binding.randomUsersRv.setAdapter(randomUsersAdapter);
+    }
+
+    private void fetchRandomUsers() {
+        RandomPeopleService randomPeopleService = new PeopleApi().createRandomPeopleService();
+        Call<List<User>> call = randomPeopleService.fetchRandomPeople();
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                randomUsersAdapter.setData(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(RandomUsersActivity.this, "Fetch Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
+++ app/src/main/java/com/improve10x/usergenerator/randomuser/RandomUsersAdapter.java
@@ -17,6 +17,10 @@ public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolde
 
     List<User> users;
 
+    public RandomUsersAdapter(List<User> users) {
+        this.users = users;
+    }
+
     void setData(List<User> users) {
         this.users = users;
         notifyDataSetChanged();
@@ -35,12 +39,12 @@ public class RandomUsersAdapter extends RecyclerView.Adapter<RandomUserViewHolde
         User user = users.get(position);
         holder.binding.nameTxt.setText(user.getName());
         holder.binding.emailTxt.setText(user.getEmail());
-        holder.binding.creditScoreTxt.setText(user.getCreditScore());
+        holder.binding.creditScoreTxt.setText(String.valueOf(user.getCreditScore()));
         holder.binding.designationTxt.setText(user.getJob());
-        holder.binding.incomeTxt.setText(user.getIncomeUsd());
-        holder.binding.locationTxt.setText(user.getAddress().getStreetAddress() + ","
-                + user.getAddress().getCity() + "," + user.getAddress().getCountryCode() + ","
-                + user.getAddress().getZipCode());
+        holder.binding.incomeTxt.setText(String.valueOf(user.getIncomeUsd()));
+        holder.binding.locationTxt.setText(user.getAddress().getStreetAddress() + ", "
+                + user.getAddress().getCity() + ", " + user.getAddress().getCountryCode() +
+                 user.getAddress().getZipCode());
     }
 
     @Override
