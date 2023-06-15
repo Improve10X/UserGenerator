c36512aded5671981801ec8a82e7fddbc1f10342
K Hareesh <hareesh.komma@improve10x.com>
2023-06-15 14:38:58 +0530

    I Implemented the save and delete functionality in UserDetailsActivity

--- app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.improve10x.usergenerator.databinding.ActivityUserDetailsBinding;
 import com.improve10x.usergenerator.model.User;
+import com.improve10x.usergenerator.network.crudnetwork.CrudUserApi;
+import com.improve10x.usergenerator.network.crudnetwork.CrudUserApiService;
+import com.improve10x.usergenerator.users.crudusers.UsersActivity;
+import com.improve10x.usergenerator.users.generatorrandomusers.GenerateRandomUsersActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
      private ActivityUserDetailsBinding binding;
      private User user;
+
+     private CrudUserApiService crudUserApiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +40,60 @@ public class UserDetailsActivity extends AppCompatActivity {
             binding.deleteBtn.setVisibility(View.INVISIBLE);
         }
         showData();
+        handleSaveBtn();
+        handleDeleteBtn();
     }
 
     private void showData() {
         binding.setUser(user);
+    }
+
+    private void createUser() {
+        CrudUserApiService crudUserApiService = new CrudUserApi().createCrudUserApiService();
+        Call<User> call = crudUserApiService.createUser(user);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Toast.makeText(UserDetailsActivity.this, "Saved SuccessFully", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(UserDetailsActivity.this, "Save Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleSaveBtn() {
+        binding.saveBtn.setOnClickListener(v -> {
+            createUser();
+        });
+    }
+
+    private void deleteUser() {
+        createCrudUserApiService();
+        Call<Void> call = crudUserApiService.deleteUser(user.getId());
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(UserDetailsActivity.this, "Successfully deleted user", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(UserDetailsActivity.this, "Failed to delete user", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleDeleteBtn() {
+        binding.deleteBtn.setOnClickListener(v -> {
+            deleteUser();
+        });
+    }
+
+    private void createCrudUserApiService() {
+        CrudUserApi api = new CrudUserApi();
+        crudUserApiService = api.createCrudUserApiService();
     }
 }
