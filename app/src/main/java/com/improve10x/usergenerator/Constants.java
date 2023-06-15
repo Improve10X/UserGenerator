b776b9d3b49d9a9a9738d7810901f95057693918
Omprakash <omprakash.mopuri@improve10x.com>
2023-06-15 13:02:35 +0530

    Modified KEY_RANDOM_USER value in Constants.java

--- app/src/main/java/com/improve10x/usergenerator/Constants.java
+++ app/src/main/java/com/improve10x/usergenerator/Constants.java
@@ -5,6 +5,5 @@ public class Constants {
     public static final String CRUD_USER_BASE_URL = "https://crudcrud.com/api/479dd07f8c1d482e9219f7dcb48e25f4/";
     public static final String RANDOM_PEOPLE_GENERATOR_BASE_URL = "https://peoplegeneratorapi.live/api/";
     public static final String KEY_USER = "user";
-
-    public static final String KEY_RANDOM_USER = "user";
+    public static final String KEY_RANDOM_USER = "randomUser";
 }
