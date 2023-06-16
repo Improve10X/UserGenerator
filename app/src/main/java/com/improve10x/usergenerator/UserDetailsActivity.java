64f21e84c68990387b6da640d7c1ba17216a4bfb
LENOVO <lakshmi.sannithi@improve10x.com>
2023-06-16 15:28:14 +0530

    I completed Deletebtn functionality in UserDetailsActivity

--- app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.improve10x.usergenerator.databinding.ActivityUserDetailsBinding;
 import com.improve10x.usergenerator.model.User;
+import com.improve10x.usergenerator.network.usersNetwork.UsersApi;
+import com.improve10x.usergenerator.network.usersNetwork.UsersApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserDetailsActivity extends AppCompatActivity {
    private ActivityUserDetailsBinding userDetailsBinding;
    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +36,27 @@ public class UserDetailsActivity extends AppCompatActivity {
             userDetailsBinding.saveBtn.setVisibility(View.VISIBLE);
         }
         userDetailsBinding.setUser(user);
+
+        userDetailsBinding.deleteBtn.setOnClickListener(view -> {
+            deleteUserDetails();
+        });
+    }
+
+
+
+    private void deleteUserDetails(){
+        UsersApiService usersApiService = new UsersApi().createUserApiService();
+        Call<Void> call = usersApiService.deleteUser(user.getId());
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(UserDetailsActivity.this, "Deleted person details", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(UserDetailsActivity.this, "Failed to delete details", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
