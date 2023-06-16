e7b0d4d21362f95da497b8998a43fe7f4e4d0b2b
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-06-16 15:01:09 +0530

    implemented buttons Visibility for User Details Activity

--- app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
@@ -3,6 +3,7 @@ package com.improve10x.usergenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.improve10x.usergenerator.databinding.ActivityUserDetailsBinding;
 import com.improve10x.usergenerator.model.User;
@@ -16,13 +17,16 @@ public class UserDetailsActivity extends AppCompatActivity {
         userDetailsBinding = ActivityUserDetailsBinding.inflate(getLayoutInflater());
         setContentView(userDetailsBinding.getRoot());
         getSupportActionBar().setTitle("User Details");
-        if(getIntent().hasExtra(Constants.KEY_RANDOM_USERS_VALUE)) {
-            user = (User) getIntent().getSerializableExtra(Constants.KEY_RANDOM_USERS_VALUE);
-            userDetailsBinding.setUser(user);
-        }
+
         if (getIntent().hasExtra("User")) {
             user = (User) getIntent().getSerializableExtra("User");
-            userDetailsBinding.setUser(user);
+            userDetailsBinding.deleteBtn.setVisibility(View.VISIBLE);
+            userDetailsBinding.saveBtn.setVisibility(View.INVISIBLE);
+        } else if(getIntent().hasExtra(Constants.KEY_RANDOM_USERS_VALUE)) {
+            user = (User) getIntent().getSerializableExtra(Constants.KEY_RANDOM_USERS_VALUE);
+            userDetailsBinding.deleteBtn.setVisibility(View.INVISIBLE);
+            userDetailsBinding.saveBtn.setVisibility(View.VISIBLE);
         }
+        userDetailsBinding.setUser(user);
     }
 }
