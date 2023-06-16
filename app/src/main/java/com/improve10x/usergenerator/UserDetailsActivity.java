00e812325eddccb8a973b44ffff9d2eb73a052ad
Harsha <shreeharsha.a@improve10x.com>
2023-06-16 15:39:42 +0530

    I implemented save button functionality for user details activity

--- app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
+++ app/src/main/java/com/improve10x/usergenerator/UserDetailsActivity.java
@@ -11,6 +11,8 @@ import com.improve10x.usergenerator.model.User;
 import com.improve10x.usergenerator.network.usersNetwork.UsersApi;
 import com.improve10x.usergenerator.network.usersNetwork.UsersApiService;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -40,9 +42,29 @@ public class UserDetailsActivity extends AppCompatActivity {
         userDetailsBinding.deleteBtn.setOnClickListener(view -> {
             deleteUserDetails();
         });
+
+        userDetailsBinding.saveBtn.setOnClickListener(v -> {
+            addUserDetails();
+        });
     }
 
+    private void addUserDetails() {
+        UsersApi usersApi = new UsersApi();
+        UsersApiService usersApiService = usersApi.createUserApiService();
+        Call <User> call = usersApiService.createUser(user);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Toast.makeText(UserDetailsActivity.this, "Successfully added the data", Toast.LENGTH_SHORT).show();
+                finish();
+            }
 
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(UserDetailsActivity.this, "Failed to add data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     private void deleteUserDetails(){
         UsersApiService usersApiService = new UsersApi().createUserApiService();
